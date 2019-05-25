172889e24df84cf7ac7b928e047e192daba5bb06
ombogdan <omelcenko44@gmail.com>
2019-05-25 12:01:39 +0300

    Добавив вход і домашню сторінку4

---
+++ menu/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+# Create your models here.
+class Pizzas(models.Model):
+    title = models.TextField(max_length=20)
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=50, decimal_places=3, default=0.0)
+    mass = models.IntegerField(default=0.0)
+    photo = models.ImageField(upload_to='pizza_images/', blank=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Drinks(models.Model):
+    title = models.TextField(max_length=20)
+    description = models.TextField(max_length=500)
+    price = models.DecimalField(max_digits=50, decimal_places=3, default=0.0)
+    ml = models.IntegerField(default=0.0)
+    photo = models.ImageField(upload_to='drinks_images/', blank=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Deserts(models.Model):
+    title = models.TextField(max_length=20)
+    price = models.DecimalField(max_digits=50, decimal_places=3, default=0.0)
+    mass = models.IntegerField(default=0.0)
+    photo = models.ImageField(upload_to='deserts_images/', blank=False)
+
+    def __str__(self):
+        return self.title
